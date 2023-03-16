ea73acb55b3446269827bd7101cabad99388d75f
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-16 15:58:32 +0100

    Upgrad palindroma.js

--- js/palidroma.js
+++ js/palidroma.js
@@ -4,3 +4,14 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 
 */
+
+//prendiamo il tag del bottone  
+const btnWord = document.getElementById('btnWord');
+
+btnWord.addEventListener('click', wordValue);
+//Inizio della funzione per prenderci la parola inserita dall'utente
+function wordValue(e){
+    e.preventDefault();
+    const wordUser = document.getElementById('word').value;
+    console.log(wordUser);
+}
