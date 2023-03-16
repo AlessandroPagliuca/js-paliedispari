ca5b20a48c4f33ea2a22b7120d975f2f8e581dbc
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-16 17:53:38 +0100

    Upgrade paridispari.js

--- js/paridispari.js
+++ js/paridispari.js
@@ -8,6 +8,38 @@ Stabiliamo se la somma dei due numeri è pari o dispari
 Dichiariamo chi ha vinto.
 */
 
+
+//DA QUI PARTONO TUTTE LE VARIABILI E FUNZIONI DELL'UTENTE
+
+//variabili numero scelto dall'utente
+let numberUser;
+
+//variabile del btn con rispettivo evento 
+let btnPardisp = document.getElementById('btnPardisp');
+
+btnPardisp.addEventListener('click', numberSelectedUser);
+
+function numberSelectedUser(){
+    numberUser = document.getElementById('numberUser').value;
+    console.log(numberUser);
+
+    //variabili e function della select pari dispari scleta dall'utente
+    let optionUser = document.getElementById('pariDisp');
+    optionUser.addEventListener('change', parDisp);
+
+    function parDisp(){
+        const result = optionUser.value;
+        console.log(result);
+    }
+}
+
+
+
+
+
+
+//DA QUI PARTONO TUTTE LE VARIABILI E FUNZIONI DEL COMPUTER
+
 //generiamo un numero random da 1 a 5 per il computer
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -18,22 +50,20 @@ function getRandomInt(min, max) {
 let numPc = getRandomInt(1, 6);
 console.log(numPc);
 
-//prendiamo in input opzione scelta dall'utente 
-let optionUser = document.getElementById('pariDisp');
-optionUser.addEventListener('change', parDisp);
-
-function parDisp(){
-    const result = optionUser.value;
-    console.log(result);
-}
-
-numUser.addEventListener('change', )
-//sommiamo i due numeri stabilendo se la somma dei due numeri
-// è pari o dispari  e dichiariamo chi ha vinto
+/*
+SOMMIAMO i due numeri stabilendo se la somma dei due numeri
+è pari o dispari  e dichiariamo chi ha vinto
+*/ 
 function pariDispari(num1, num2){
     const somma = num1 + num2;
     if(somma % 2 === 0){
+        const text = document.querySelector('h3').innerHTML = 'Hai vinto';
+        console.log(text);
         return true;
     }
+    const text = document.querySelector('h3').innerHTML = 'Hai  perso';
+    console.log(text);
     return false
 }
+
+let result = pariDispari(numberSelectedUser, numPc)
