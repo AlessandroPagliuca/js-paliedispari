5d4faae0c3b1f5297d287c6d888acdee6659c888
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-16 17:09:28 +0100

    Upgarde paridispari.js

--- js/paridispari.js
+++ js/paridispari.js
@@ -13,14 +13,27 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
+}
 
-  //sommiamo i due numeri stabilendo se la somma dei due numeri
-  // è pari o dispari  e dichiariamo chi ha vinto
-  function pariDispari(num1, num2){
+let numPc = getRandomInt(1, 6);
+console.log(numPc);
+
+//prendiamo in input opzione scelta dall'utente 
+let optionUser = document.getElementById('pariDisp');
+optionUser.addEventListener('change', parDisp);
+
+function parDisp(){
+    const result = optionUser.value;
+    console.log(result);
+}
+
+numUser.addEventListener('change', )
+//sommiamo i due numeri stabilendo se la somma dei due numeri
+// è pari o dispari  e dichiariamo chi ha vinto
+function pariDispari(num1, num2){
     const somma = num1 + num2;
     if(somma % 2 === 0){
         return true;
     }
     return false
-  }
+}
