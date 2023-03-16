71716ea61c1467d256db9860b24226540d940ac6
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-16 16:43:01 +0100

    Upgrade paridispari.js

--- js/paridispari.js
+++ js/paridispari.js
@@ -7,3 +7,20 @@ Stabiliamo se la somma dei due numeri è pari o dispari
 (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+//generiamo un numero random da 1 a 5 per il computer
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  //sommiamo i due numeri stabilendo se la somma dei due numeri
+  // è pari o dispari  e dichiariamo chi ha vinto
+  function pariDispari(num1, num2){
+    const somma = num1 + num2;
+    if(somma % 2 === 0){
+        return true;
+    }
+    return false
+  }
