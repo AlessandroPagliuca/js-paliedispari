e5c5ad6ba428a2c0fb694082cf2d45dbe4a3b823
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-16 16:12:18 +0100

    Upgrade palindroma.js

--- js/palidroma.js
+++ js/palidroma.js
@@ -6,12 +6,37 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 //prendiamo il tag del bottone  
+let wordUser;
 const btnWord = document.getElementById('btnWord');
 
 btnWord.addEventListener('click', wordValue);
 //Inizio della funzione per prenderci la parola inserita dall'utente
 function wordValue(e){
     e.preventDefault();
-    const wordUser = document.getElementById('word').value;
+    wordUser = document.getElementById('word').value;
     console.log(wordUser);
+
+    
+    // converte la parola in minuscolo e rimuove gli spazi bianchi
+    wordUser = wordUser.toLowerCase();
+    console.log(wordUser)
+        
+    // converte la parola in un array di caratteri e inverte l'array
+    let reversedWord = wordUser.split("").reverse().join("");
+    console.log(reversedWord);
+
+    // confronta la parola originale con la versione invertita
+    if (wordUser === reversedWord) {
+        console.log(true);
+        return true;
+
+    } else {
+        console.log(false)
+        return false;
+
+    }
 }
+
+
+
+  
